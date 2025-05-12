fb136f262c058b7976ec2f6d5fc062191e85f1b2
1hpecvns <ihpecvns@gmail.com>
2025-05-12 17:21:28 +0800

    重构template.cpp，添加快速输入输出函数，优化main函数结构以提升代码性能和可读性

--- template.cpp
+++ template.cpp
@@ -1,26 +1,62 @@
-#include <algorithm>
-#include <iostream>
-#include <print>
-#include <ranges>
+#include <cmath>
+#include <cstdio>
+
+const int N_io = 1e4 + 10;
+
+char *p1, *p2, buf[N_io];                                            // NOLINT
+[[nodiscard]] inline char nc() noexcept {                            // NOLINT
+  return (p1 == p2 && (p2 = (p1 = buf) + fread(buf, 1, N_io, stdin), // NOLINT
+                       p1 == p2)                                     // NOLINT
+              ? EOF
+              : *p1++); // NOLINT
+}
+int read() {                         // NOLINT
+  int x = 0;                         // NOLINT
+  char ch = nc();                    // NOLINT
+  while (ch < 48 || ch > 57)         // NOLINT
+    ch = nc();                       // NOLINT
+  while (ch >= 48 && ch <= 57)       // NOLINT
+    x = x * 10 + ch - 48, ch = nc(); // NOLINT
+  return x;                          // NOLINT
+}
+
+char obuf[N_io], *p3 = obuf;                                        // NOLINT
+inline void pc(char x) {                                            // NOLINT
+  (p3 - obuf < N_io)                                                // NOLINT
+      ? (*p3++ = x)                                                 // NOLINT
+      : (fwrite(obuf, p3 - obuf, 1, stdout), p3 = obuf, *p3++ = x); // NOLINT
+}
+inline void write(int x) { // NOLINT
+  if (!x) {                // NOLINT
+    pc('0');               // NOLINT
+    return;                // NOLINT
+  } // NOLINT
+  int len = 0, k1 = x, c[40];          // NOLINT
+  if (k1 < 0)                          // NOLINT
+    k1 = -k1, pc('-');                 // NOLINT
+  while (k1)                           // NOLINT
+    c[len++] = k1 % 10 ^ 48, k1 /= 10; // NOLINT
+  while (len--)                        // NOLINT
+    pc(c[len]);                        // NOLINT
+}
+struct iofush { // NOLINT
+  ~iofush() {
+    fwrite(obuf, p3 - obuf, 1, stdout); // NOLINT
+  }
+} ioflush1; // NOLINT
+// 以上为快读快写
+
 #include <vector>
 
-#define ts views::transform
-#define ia views::iota
-#define vc vector
-#define to ranges::to
-#define en enumerate
-#define is views::istream
-#define rs ranges::sort
+namespace {
+using namespace std; // NOLINT
+auto solve() {}
+} // namespace
 
-using namespace std;
-using namespace views;
-
-auto main() -> int {
-  ios::sync_with_stdio(false), cin.tie(nullptr);
-  int n;
-  auto a = is<int>(cin) | to<vc>();
-  rs(a);
-#pragma unroll 4
-  for (auto &&[i, x] : en(a))
-    cout << x << " \n"[i == n - 1];
+auto main() noexcept -> int {
+  try {
+    solve();
+  } catch (...) {
+    return 0;
+  }
 }
